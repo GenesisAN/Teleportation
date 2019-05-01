4f5621718f57ab729ea1c21f3b093ab83274396d
Enes Sadık Özbek <es.ozbek@outlook.com>
2019-05-02 01:33:47 +0300

    Update to latest RocketMod

---
+++ src/Properties/ServiceConfigurator.cs
@@ -0,0 +1,13 @@
+﻿using RestoreMonarchy.TeleportationPlugin.Requests;
+using Rocket.API.DependencyInjection;
+
+namespace RestoreMonarchy.TeleportationPlugin.Properties
+{
+    public class ServiceConfigurator : IServiceConfigurator
+    {
+        public void ConfigureServices(IDependencyContainer container)
+        {
+            container.AddSingleton<ITeleportationRequestManager, TeleportationRequestManager>();
+        }
+    }
+}
